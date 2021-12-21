71e9459b46c36f0a83ffb085e6b48cd7f1c94973
Mexil77 <mendel_77@hotmail.com>
2021-12-21 16:51:29 +0100

    Corrigiendo norma

--- srcs/builtins/ft_builtins.c
+++ srcs/builtins/ft_builtins.c
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 18:00:15 by emgarcia          #+#    #+#             */
-/*   Updated: 2021/12/21 16:38:19 by emgarcia         ###   ########.fr       */
+/*   Updated: 2021/12/21 16:50:40 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 void	ft_builtins(t_general *g)
 {
 	size_t	i;
-	//char	**cmd;
 
 	i = -1;
 	while (++i < g->argssize)
 	{
 		if (g->args[i].type == 3)
 		{
-			//cmd = ft_split(g->args[i].content, ' ');
 			if (!ft_strncmp(g->args[i].content[0], "cd", 2))
 				ft_cd(&g->ownenv, g->args[i].content[1]);
 			else if (!ft_strncmp(g->args[i].content[0], "unset", 4)
