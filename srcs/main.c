45548089e7d71425bbfb938cd2058560d0901beb
Mexil77 <mendel_77@hotmail.com>
2021-12-21 15:44:24 +0100

    nada

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 18:13:22 by emgarcia          #+#    #+#             */
-/*   Updated: 2021/12/21 14:49:36 by emgarcia         ###   ########.fr       */
+/*   Updated: 2021/12/21 14:50:02 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ void	ft_prompt(t_general *g)
 			ft_inigeneral(g);
 			ft_parse(g, command);
 			ft_executor(g);
-			//ft_printgeneral(g);
+			ft_printgeneral(g);
 			ft_freegeneral(g);
 		}
 	}
